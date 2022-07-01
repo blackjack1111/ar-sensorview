85c1796483a8a522399281fdfd8cca850be19650
Jakob PC <jakob.gromer@tum.de>
2022-07-01 22:58:50 +0200

    Added basic sensor scene

--- Assets/Scripts/Sensor.cs
+++ Assets/Scripts/Sensor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReadData : MonoBehaviour
+public class Sensor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -10,9 +10,14 @@ public class ReadData : MonoBehaviour
         
     }
 
+    public float scanRate = 4.0f;
+
     // Update is called once per frame
     void Update()
     {
-        transform.position()
+        float angle = 360.0f / scanRate * Time.deltaTime;
+        transform.RotateAround(transform.position, new Vector3(0,-1,0), angle);
     }
+
+    
 }
